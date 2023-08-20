5fb89a7962e02b935d7cfb31b175376890024548
8kng <halkgonke278@gmail.com>
2023-08-20 18:36:11 +0900

    最初のコミット

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.myapplication
+
+import android.content.ContentValues
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(ContentValues.TAG, "onStart()")
+
+        binding.ARStartButton.setOnClickListener{
+            //val intent = Intent(applicationContext, AugmentedImageActivity::class.java)
+            //startActivity(intent)
+        }
+        binding.imageSaveButton.setOnClickListener{
+            val intent = Intent(applicationContext, SecondActivity::class.java)
+            startActivity(intent)
+            Toast.makeText(this, "yeah", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
